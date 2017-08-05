3b239f78d910c99c94df2b89d6a8435f41520f2d
Sriram Narayanan <sriramnrn@gmail.com>
2017-08-05 19:57:00 +0800

    Update fetch_illumos_closed_bins.sh

--- fetch_illumos_closed_bins.sh
+++ fetch_illumos_closed_bins.sh
@@ -3,8 +3,8 @@
 ON_CLOSED_BINS_URL="https://download.joyent.com/pub/build/illumos/on-closed-bins.i386.tar.bz2"
 ON_CLOSED_BINS_ND_URL="https://download.joyent.com/pub/build/illumos/on-closed-bins-nd.i386.tar.bz2"
 
-local cld="on-closed-bin.i386.tar.bz2"
-local clnd="on-closed-bin-nd.i386.tar.bz2"
+cld="on-closed-bin.i386.tar.bz2"
+clnd="on-closed-bin-nd.i386.tar.bz2"
 
 curl -k $ON_CLOSED_BINS_URL -o $cld
 curl -k $ON_CLOSED_BINS_ND_URL -o $clnd
